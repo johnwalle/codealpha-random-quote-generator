addfb1e11af81be6426a68e0cffe0f8f3b6668db
mark133227 <etawe1627@gmail.com>
2025-06-21 09:11:20 -1200

    Add curated list of motivational quotes

---
+++ src/utils/quotes.js
@@ -0,0 +1,25 @@
+// utils/quotes.js
+const quotes = [
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs",
+  },
+  {
+    text: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
+  },
+  {
+    text: "Do one thing every day that scares you.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    text: "Happiness is not something ready made. It comes from your own actions.",
+    author: "Dalai Lama",
+  },
+];
+
+export default quotes;
